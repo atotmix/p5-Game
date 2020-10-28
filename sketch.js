ee3033b2a5c89baa7aab788bf55aee3f35cd064c
unknown <carter.emms@ada.ac.uk>
2020-10-28 09:46:39 +0000

    Maths question generator

--- sketch.js
+++ sketch.js
@@ -1,5 +1,26 @@
+var highScore;
+
+
 function preload(){
-  // put preload code here
+  highScore = getItem('highScore');
+   if (highScore === null) {
+     highScore = 0;
+   }
+
+}
+
+function questionGen(difficulty) {
+  operator = ["+","-","*","/"][Math.floor(Math.random()*4)];
+  if (operator == "/"){
+    a = Math.floor(Math.random() * (10+difficulty)) + 1;
+    b = a * Math.floor(Math.random() * 10) + a;
+    return (b + operator + a);
+  }
+  else{
+    a = Math.floor(Math.random() * (10+difficulty)) + 1;
+    b = Math.floor(Math.random() * (10+difficulty)) + 1;
+    return (a + operator + b);
+  }
 }
 
 function setup() {
@@ -8,5 +29,5 @@ function setup() {
 }
 
 function draw() {
-  // put drawing code here
+  console.log(eval(questionGen(4)));
 }
